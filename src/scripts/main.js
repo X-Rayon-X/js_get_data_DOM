78c235af8777263aceda42866e2c988a0a8fbd0a
Nazar Bakay <bakayn484@gmail.com>
2025-05-15 19:40:35 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,16 +6,18 @@ const values = document.getElementsByClassName('population');
 
 let resAverage = 0;
 let resTotal = 0;
+let count = 0;
 
 for (const value of values) {
   const cleanValue = value.textContent.replace(/,/g, '');
 
   if (!isNaN(Number(cleanValue))) {
     resTotal += +cleanValue;
+    count++;
   }
 }
 
-resAverage = resTotal / values.length;
+resAverage = resTotal / count;
 
 const formattedAverage = resAverage.toLocaleString();
 const formattedTotal = resTotal.toLocaleString();
